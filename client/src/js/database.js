312b791ce8548c254360ed0f171c063db3125bcb
kalina <kalina.boksa@gmail.com>
2023-01-06 22:50:33 -0600

    fix db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,35 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("Put to the database");
-  //connect to DB and version we want to use
-  const textDb = await openDB("text", 1);
-  //make new transaction , specify the db we posting to and the data privilege of "readwrite"
-  const tx = textDb.transaction("text", "readwrite");
-  //open object store
-  const store = tx.objectStore("text");
-  //use .put() method to pass in content
-  const request = store.put({ text: content });
-  //confirm teh data was added
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ jate: content });
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  console.log("🚀 - data saved to the database:", result);
 };
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => {
-  console.log(`GET all from the database`);
-  //connect to DB and version we want to use
-  const todosDb = await openDB("text", 1);
-  // make new transaction , specify the db we posting to and the data privilege of "readonly"
-  const tx = todosDb.transaction("text", "readonly");
-  //open the object store
-  const store = tx.objectStore("text");
-  //use .getAll() method to grab all the content in the DB
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const request = store.getAll();
-  //confirm the data was fetched
   const result = await request;
-  console.log("result.value", result);
-  // return result.value;
+  console.log("result value: ", result);
+  return result[result.length - 1].jate;
 };
+
 initdb();
